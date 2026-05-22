c4f41c331393daa0ed7fe0f6bd34cb3c6e37a37e
Thomas Harrison <theharrisoncrafter@gmail.com>
2026-05-22 18:59:37 -0400

    Rewrite demo

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,21 +1,28 @@
-extern crate lib_dice;
+use std::str::FromStr;
 
-use std::io;
-use std::io::prelude::*;
+use lib_dice::DiceRoll;
 
 fn main() {
-    println!("Welcome to Dice Line v{}", env!("CARGO_PKG_VERSION"));
+    let roll_1 = lib_dice::roll(1, 8, 0); // 1d8 + 0
+    let roll_2 = lib_dice::roll_from_str("2d6 + 7").unwrap();
 
-    loop {
-        print!("> ");
-        io::stdout().flush().unwrap();
+    println!("Roll 1 is {} roll 2 is {}", roll_1, roll_2);
 
-        let mut line = String::new();
-        std::io::stdin().read_line(&mut line).expect("STDIN Error");
+    let dice = DiceRoll::new(1, 8, 3); // 1d8 + 3
+    let roll_3 = dice.roll();
+    let roll_4 = dice.roll_no_bonus();
 
-        match lib_dice::roll_from_str(&line.trim()) {
-            Ok(res) => println!("{}", res),
-            Err(err) => println!("Error: {}", err),
-        }
+    println!(
+        "Dice {} rolled 2 times and got [{}, {}]",
+        dice, roll_3, roll_4
+    );
+
+    let s = "3d12+5";
+    let dice = DiceRoll::from_str(s).unwrap();
+    print!("Rolling {} got", dice);
+    for _ in 0..5 {
+        let roll = dice.roll();
+        print!(" {}", roll);
     }
+    println!("");
 }
